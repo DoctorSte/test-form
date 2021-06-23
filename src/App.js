b2e38afebb4d278e09f11b99a19a9f5ba2d7eea7
DoctorSte <st.dobrescu@gmail.com>
2021-06-23 20:53:17 +0100

    Data changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       
       <Contact />
+      <iframe title="airtable" class="airtable-embed" src="https://airtable.com/embed/shrR3MnZycYg7gk9s?backgroundColor=red&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" ></iframe>
     </div>
   );
 }
